c95eb5bd7aba0926996e65574a8ea272b73d53c5
Shreyas Sane <shreyasdbz@gmail.com>
2024-04-24 15:38:17 -0400

    Initial Commit

---
+++ SplitMyMeal-Prod-A/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SplitMyMeal-Prod-A
+//
+//  Created by Shreyas Sane on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplitMyMeal-Prod-A/SplitMyMeal_Prod_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitMyMeal_Prod_AApp.swift
+//  SplitMyMeal-Prod-A
+//
+//  Created by Shreyas Sane on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitMyMeal_Prod_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
